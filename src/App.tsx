382e0c25451342e9e3b7ec16966f59cb74c29659
Roman Kh <strex.gamer91@gmail.com>
2026-01-29 17:45:16 +0500

    Constatns file & Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import NavBar from "@/components/NavBar";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Welcome to the portfolio</h1>
+    <main>
+      <NavBar />
+    </main>
   );
 };
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,34 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "@/constants";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="">
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Roman's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
